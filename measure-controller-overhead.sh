9d6ba5e97295307950c3ad86a8bc27bef7ccd9ac
Andreea Pocol <andreeapocol@ymail.com>
2021-11-11 10:42:15 -0500

    don't delete capture files until beginning of next experiment

--- measure-controller-overhead.sh
+++ measure-controller-overhead.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-num_experiments=1
-pox_csv_file=~/mininet-flow-generator/results_pox_controller.csv
-def_csv_file=~/mininet-flow-generator/results_default_controller.csv
 # export EXPDUR=36
 # export EL_FLOWS=5
 # export MI_FLOWS=45
+
+num_experiments=1
+
+pox_csv_file=~/mininet-flow-generator/results_pox_controller.csv
+def_csv_file=~/mininet-flow-generator/results_default_controller.csv
+captureFileDefaultController=/media/sf_Shared/default.pcap
+captureFilePoxController=/media/sf_Shared/pox.pcap
 
 sudo rm -rf /home/mininet/Desktop/mininet-log/test*
 sudo rm -f $pox_csv_file &> /dev/null
@@ -16,13 +20,15 @@ sudo touch $def_csv_file
 
 for (( n=0; n<$num_experiments; n++ ))
 do
+    sudo rm $captureFileDefaultController &> /dev/null
+    sudo rm $captureFilePoxController &> /dev/null
+
     cd ~/pox
     sudo ./pox.py forwarding.l2_learning host_tracker openflow.discovery openflow.of_01 --port=6633 &
     echo "********** POX CONTROLLER STARTED **********"
 
     cd ~/mininet-flow-generator
-    filename=/media/sf_Shared/pox.pcap
-    # export FLNM=$filename
+    # export FLNM=$captureFilePoxController
     sudo mn --clean &> /dev/null
     echo "********** CLEANUP COMPLETE **********"
     echo "********** STARTING TRAFFIC **********"
@@ -44,19 +50,16 @@ do
     '
 
     sleep 2
-    ls $filename
     echo "********** TSHARK FINISHED **********"
 
     sudo pkill -9 -f pox*
     echo "********** POX CONTROLLER KILLED **********"
 
     sudo echo "Run $n" >> $pox_csv_file
-    sudo python stats.py $filename >> $pox_csv_file &
+    sudo python stats.py $captureFilePoxController >> $pox_csv_file &
     echo "********** POX CONTROLLER RESULTS SAVED TO FILE **********"
-    sudo rm $filename &> /dev/null
 
-    filename=/media/sf_Shared/default.pcap
-    # export FLNM=$filename
+    # export FLNM=$captureFileDefaultController
     sudo mn --clean &> /dev/null
     echo "********** CLEANUP COMPLETE **********"
     echo "********** STARTING TRAFFIC **********"
@@ -77,13 +80,12 @@ do
         expect eof
     '
     sleep 2
-    ls $filename
     echo "********** TSHARK FINISHED **********"
 
     sudo echo "Run $n" >> $def_csv_file
-    sudo python stats.py $filename >> $def_csv_file &
+    sudo python stats.py $captureFileDefaultController >> $def_csv_file &
     echo "********** DEFAULT CONTROLLER RESULTS SAVED TO FILE **********"
-    sudo rm $filename &> /dev/null
+
     sudo mn --clean &> /dev/null
 
     echo "********** ITERATION $n COMPLETE **********"
